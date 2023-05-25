94cf2e6a7ff6c37c3c695d8d1f7051b47c27dfea
Daniel <133761829+Droidenn@users.noreply.github.com>
2023-05-25 10:24:01 +0000

    Changeing "handlers_test.go" and "handlers.go". Changing "Hello everyone" to "Hello yall!" in both

--- server/handlers_test.go
+++ server/handlers_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestHelloHandler(t *testing.T) {
 	wantCode := http.StatusOK
-	wantBody := "Hello, everyone!"
+	wantBody := "Hello, yall!"
 
 	srv := &server{
 		httpServer: &http.Server{
